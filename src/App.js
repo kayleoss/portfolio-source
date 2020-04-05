2ee7edf3034ad1be7e96e3b9e4fa829e891e4469
Katie L <katie_acting@live.ca>
2020-04-05 12:18:49 -0400

    add basename to router

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Router>
+        <Router basename={process.env.PUBLIC_URL}>
           <div>
               <Route exact path="/" component={Main} />
               <Route path="/blog" component={Blog} />
