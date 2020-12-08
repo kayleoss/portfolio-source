b09eafb2bfe9286766aa86e7da0c5bb021c5d11e
Duo Liu <duoliu@Duos-MacBook-Pro.local>
2020-12-08 18:29:37 -0500

    embed video

--- src/Components/Blog/Posts/Post/index.js
+++ src/Components/Blog/Posts/Post/index.js
@@ -4,14 +4,27 @@ const Post = (props) => {
     const date = new Date(props.pubDate.split(" ")[0]);
     const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     const month = monthNames[date.getMonth()]
-    return(
-        <div className={"border box-shadow " + props.column}>
-            <a href={props.link} target='_blank'><p className="description-h m-t" dangerouslySetInnerHTML={{__html: props.title}}></p></a>
-            <p className="title-h1">{month + " " + date.getFullYear()}</p>
-            <p className="text-secondary content-post" dangerouslySetInnerHTML={{__html: props.content}}></p>
-        </div>
-    )
-    
+    const videos = ["https://medium.com/media/ea38ffc44cffc251dc54494f821bac41/href"]
+
+    let post;
+    videos.map(video => {
+        if (props.content.includes(video)) {
+            return post = 
+            <div className={"border box-shadow " + props.column}>
+                <a href={props.link} target='_blank'><p className="description-h m-t" dangerouslySetInnerHTML={{__html: props.title}}></p></a>
+                <p className="title-h1">{month + " " + date.getFullYear()}</p>
+                <iframe title={"Embed video from katie medium " + video} width={props.column === "col-sm-6" ? "450" : "300"} height="315" src={video} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
+            </div>
+        } else {
+            return post = 
+            <div className={"border box-shadow " + props.column}>
+                <a href={props.link} target='_blank'><p className="description-h m-t" dangerouslySetInnerHTML={{__html: props.title}}></p></a>
+                <p className="title-h1">{month + " " + date.getFullYear()}</p>
+                <p className="text-secondary content-post" dangerouslySetInnerHTML={{__html: props.content}}></p>
+            </div> 
+        }
+    })
+    return post
 }
 
 export default Post;
